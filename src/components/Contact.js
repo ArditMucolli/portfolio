083a7bf9c51fed88b484b05f9a8e60bc5fb0c05a
mucolliardit@gmail.com <mucolliardit@gmail.com>
2022-12-01 22:51:46 +0100

    Some change in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,4 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
-import * as Yup from "yup";
-
-const ContactSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  email: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  message: Yup.string()
-    .min(2, "Too Short!")
-    .max(200, "Too Long!")
-    .required("Required"),
-});
 
 const Contact = () => {
   return (
@@ -31,47 +14,36 @@ const Contact = () => {
           <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
         <div className="flex justify-center items-center">
-          <Formik
-            initialValues={{
-              name: "",
-              email: "",
-              message: "",
-            }}
-            validationSchema={ContactSchema}
+          <form
+            action="https://formspree.io/f/xpzngogn"
+            method="POST"
+            className="flex flex-col w-full md:w-1/2"
           >
-            {({ errors, touched }) => (
-              <Form className="flex flex-col w-full md:w-1/2">
-                <Field
-                  name="name"
-                  placeholder="Enter your full name"
-                  className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-                />
-                {errors.name && touched.name ? <div>{errors.name}</div> : null}
-                <Field
-                  name="email"
-                  type="email"
-                  placeholder="Enter your email"
-                  className="my-5 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-                />
-                {errors.email && touched.email ? (
-                  <div>{errors.email}</div>
-                ) : null}
-                <Field
-                  name="message"
-                  as="textarea"
-                  placeholder="Enter your message"
-                  rows="10"
-                  className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-                />
-                {errors.message && touched.message ? (
-                  <div>{errors.message}</div>
-                ) : null}
-                <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-                  Let's talk
-                </button>
-              </Form>
-            )}
-          </Formik>
+            <input
+              name="name"
+              type="text"
+              required
+              placeholder="Enter your full name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              name="email"
+              type="email"
+              required
+              placeholder="Enter your email"
+              className="my-5 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              required
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's talk
+            </button>
+          </form>
         </div>
       </div>
     </div>
